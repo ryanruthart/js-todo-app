b6eef9f9d5178eee198eb5d0dd31400753a58677
Ryan Ruthart <ryanruthart@gmail.com>
2019-10-15 09:34:56 -0400

    render main page view

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+// Initialize the express serving library
 const express = require('express')
 const app = express()
 
+// Set the app to serve our public folder
+app.use(express.static('public'))
+
+// Set the folder for the views
+app.set('views', 'views')
+// set the javascript view rendering engine
+app.set('view engine', 'ejs')
+
+
+// View for root 
 app.get('/', function(req, res){
-    res.send("Welcome to the new app")
+    res.render('home-guest')
 })
+
 
 app.listen(3000)
