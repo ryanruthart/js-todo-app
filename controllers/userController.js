368687242b8e23f4c1b9c43d32a3efdd74587aeb
Ryan Ruthart <ryanruthart@gmail.com>
2019-10-18 11:23:48 -0400

    post now show user name and avatar

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,9 +68,17 @@ exports.mustBeLoggedIn = function (req, res, next) {
 }
 
 exports.ifUserExists = function (req, res, next) {
-  next()
+  User.findByUsername(req.params.username).then(function(userDocument) {
+    req.profileUser = userDocument
+    next()
+  }).catch(function() {
+    res.render('404')
+  })
 }
 
 exports.profilePostsScreen = function(req, res) {
-  res.render('profile')
+  res.render('profile', {
+    profileUsername: req.profileUser.username,
+    profileAvatar: req.profileUser.avatar
+  })
 }
