20aec032f52f18dd455f1e6b215683cd03ce1672
Ryan Ruthart <ryanruthart@gmail.com>
2019-10-18 00:13:20 -0400

    updated posts to retrieve posting user

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 const postsCollection = require('../db').db().collection('posts')
 const ObjectID = require('mongodb').ObjectID
+const User = require('./User')
 
 let Post = function(data, userid) {
   this.data = data
@@ -50,9 +51,30 @@ Post.findSingleById = function(id) {
       reject()
       return
     }
-    let post = await postsCollection.findOne({_id: new ObjectID(id)})
-    if (post) {
-      resolve(post)
+
+    // get the post and get the author of it using aggregate
+    let posts = await postsCollection.aggregate([
+      {$match: {_id: new ObjectID(id)}},
+      {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
+      {$project: {
+        title:1,
+        body: 1,
+        createdDate: 1,
+        author: {$arrayElemAt: ["$authorDocument", 0]}
+      }}
+    ]).toArray()
+
+    // clean up the author property in each post object
+    posts = posts.map(function(post) {
+      post.author = {
+        username: post.author.username,
+        avatar: new User(post.author, true).avatar
+      }
+      return post
+    })
+
+    if (posts.length) {
+      resolve(posts[0])
     } else {
       reject()
     }
